44a1acd678fa48adf02c8e77ce22d20bc8c344d3
David-Koschmann <45025444+David-Koschmann@users.noreply.github.com>
2026-07-16 11:55:01 +1000

    Codewars 8kyu - Jenny's secret message

---
+++ Codewars/8kyu/jenny's_secret_message.py
@@ -0,0 +1,6 @@
+# Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different.
+
+def greet(name):
+    if name == "Johnny":
+        return f"Hello, my love!"
+    return f"Hello, {name}!"
